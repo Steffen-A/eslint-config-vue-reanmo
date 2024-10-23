07670f123f73a568d29ff1ba9826f282ec4006c0
Steffen-A <56999699+Steffen-A@users.noreply.github.com>
2024-10-23 13:55:51 +0200

    feature: adjust rules

--- README.md
+++ README.md
@@ -1,21 +1,22 @@
 # eslint-config-vue-reanmo
 
-> **Sharable ESLint config with TypeScript, Vue, and Prettier for ESLint 9+**
+> **Sharable ESLint config with TypeScript, Vue, Prettier, and Tailwind CSS for ESLint 9+**
 
-This package provides a sharable ESLint configuration for projects using **Vue** and **TypeScript**, along with **Prettier** integration. It includes sensible defaults and strict linting rules for better code quality and consistency.
+This package provides a shareable ESLint configuration for projects using **Vue** and **TypeScript**, along with **Prettier** integration. It includes sensible defaults and strict linting rules to improve code quality and consistency.
 
 ## Features
 
 - **ESLint 9+**: Compatible with the latest version of ESLint.
-- **Vue Support**: Integrates `eslint-plugin-vue` with recommended settings for Vue files.
+- **Vue Support**: Integrates `eslint-plugin-vue` with recommended and custom settings for Vue files.
 - **TypeScript Support**: Configures `@typescript-eslint` for stricter type safety and better code style enforcement.
-- **Unused Imports**: Automatically removes unused imports with `eslint-plugin-unused-imports`.
+- **Tailwind CSS Support**: Uses `eslint-plugin-tailwindcss` exclusively to enforce the correct order of Tailwind CSS classes.
 - **Prettier Integration**: Uses `eslint-config-prettier` to avoid conflicts between Prettier and ESLint formatting rules.
-- **Custom Rules**: Provides additional custom rules, like enforcing PascalCase for Vue components and stricter handling of unused variables.
+- **Unused Imports**: Automatically removes unused imports with `eslint-plugin-unused-imports`.
 
 ## Installation
 
 First, install the package and its peer dependencies:
 
 ```bash
-npm install eslint-config-vue-reanmo
+npm install @steffen-reanmo/eslint-config-vue eslint prettier typescript eslint-plugin-vue typescript-eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint-plugin-tailwindcss eslint-plugin-unused-imports --save-dev
+```
--- index.js
+++ index.js
@@ -1,52 +1,89 @@
-import globals from 'globals';
-import eslint from '@eslint/js';
-import pluginVue from 'eslint-plugin-vue';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import tseslint from 'typescript-eslint';
-import unusedImports from 'eslint-plugin-unused-imports';
+import globals from "globals";
+import eslint from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+import unusedImports from "eslint-plugin-unused-imports";
+import tailwind from "eslint-plugin-tailwindcss";
 
 export default [
-    {
-        plugins: {
-            'unused-imports': unusedImports,
-        },
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+      tailwindcss: tailwind,
     },
-    { ignores: ['dist'] },
-    { files: ['src/**/*.{js,mjs,cjs,ts,vue}'] },
-    eslint.configs.recommended,
-    ...tseslint.configs.strict,
-    ...tseslint.configs.stylistic,
-    ...pluginVue.configs['flat/recommended'],
-    {
-        files: ['*.vue', '**/*.vue'],
-        languageOptions: {
-            parserOptions: {
-                parser: '@typescript-eslint/parser',
-            },
-        },
+  },
+  { ignores: ["dist"] },
+  { files: ["src/**/*.{js,mjs,cjs,ts,vue}"] },
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  ...pluginVue.configs["flat/recommended"],
+  {
+    files: ["*.vue", "**/*.vue"],
+    languageOptions: {
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
     },
-    {
-        rules: {
-            'vue/component-name-in-template-casing': [
-                'error',
-                'PascalCase',
-                {
-                    registeredComponentsOnly: false,
-                },
-            ],
-            'no-unused-vars': 'off',
-            'unused-imports/no-unused-imports': 'error',
-            'unused-imports/no-unused-vars': [
-                'warn',
-                {
-                    vars: 'all',
-                    varsIgnorePattern: '^_',
-                    args: 'after-used',
-                    argsIgnorePattern: '^_',
-                },
-            ],
+  },
+  {
+    rules: {
+      "vue/component-name-in-template-casing": [
+        "error",
+        "PascalCase",
+        {
+          registeredComponentsOnly: false,
         },
+      ],
+      "vue/block-lang": [
+        "error",
+        {
+          script: {
+            lang: "ts",
+          },
+        },
+      ],
+      "vue/block-order": [
+        "error",
+        {
+          order: ["template", "script", "style"],
+        },
+      ],
+      "vue/component-api-style": ["error", ["script-setup", "composition"]],
+      "vue/define-emits-declaration": ["error", "type-literal"],
+      "vue/define-macros-order": [
+        "error",
+        {
+          order: ["defineOptions", "defineProps", "defineEmits"],
+          defineExposeLast: true,
+        },
+      ],
+      "vue/define-props-declaration": ["error", "type-based"],
+      "vue/no-root-v-if": ["error"],
+      "vue/no-undef-components": [
+        "error",
+        {
+          ignorePatterns: [],
+        },
+      ],
+      "vue/padding-line-between-blocks": ["error", "always"],
+      "vue/prefer-separate-static-class": ["error"],
+      "vue/require-typed-ref": ["error"],
+      "tailwindcss/classnames-order": ["error"],
+      "no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
     },
-    { languageOptions: { globals: globals.browser } },
-    eslintConfigPrettier,
+  },
+  { languageOptions: { globals: globals.browser } },
+  eslintConfigPrettier,
 ];
